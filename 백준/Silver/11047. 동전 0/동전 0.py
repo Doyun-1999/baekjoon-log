75c37ae7903d43e41fc64b06e14dd3f12034c5fa
Doyun Joung <128355648+Doyun-1999@users.noreply.github.com>
2025-10-27 16:31:47 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,11 @@
-N, target = map(int,input().split())
+N, K = map(int,input().split())
 coins = [int(input()) for _ in range(N)]
-answer = 0
-
 coins.sort(reverse = True)
-# 이거 무슨 대표문제인데.. 그리디였나?
-# 특정 단위에서 나눠지면 해당 나머지로
+
+answer = 0
 for c in coins:
-    if target // c > 0:
-        answer += target // c
-        target = target % c
+    if K // c > 0:
+        answer += K // c
+        K = K % c
+        
 print(answer)
