c789cf137f286edadc92d0a73678a2fd82d2c90c
Doyun Joung <128355648+Doyun-1999@users.noreply.github.com>
2025-10-29 09:01:07 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,12 @@
+n = int(input())
+p = list(map(int,(input().split()))) #3 1 4 3 2
+sum = 0
+wait = 0
+p.sort()
+
+result = 0
+for i in p:
+    wait += i # wait = wait +1= 1 -> wait = wait +2= 3->wit+
+    sum += wait #sum=1 ->sum+3=4->sum+
+
+print(sum)
