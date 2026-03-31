47e121a2fbe69cc87d2ef4b300be75e36ab077aa
Doyun Joung <128355648+Doyun-1999@users.noreply.github.com>
2026-03-31 09:00:37 +0900

    [level 1] Title: 폰켓몬, Time: 1.13 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 9.54 MB, 시간: 3.06 ms
+메모리: 10 MB, 시간: 1.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 30일 23:10:06
+2026년 03월 31일 09:00:34
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,19 +1,11 @@
-## 각 배열의 요소가 중복처리를 해야함 -> 해시맵
-
 def solution(nums):
-    
-    d = {}
-    cnt = 0
+    s = set()
+    m = len(nums)//2
     
     for n in nums:
-        d[n] = d.get(n, 0) + 1
+        s.add(n)
     
-    for key in d.keys():
-        cnt += 1
-        
-    if cnt >= len(nums)//2 :
-        answer = len(nums)//2
+    if m <= len(s) :
+        return m
     else:
-        answer = cnt
-        
-    return answer
+        return len(s)
