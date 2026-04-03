aab2d997373064221508cbbe6a4722ddb82f97d6
Doyun Joung <128355648+Doyun-1999@users.noreply.github.com>
2026-04-03 23:14:58 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.02 ms
+메모리: 9.32 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 24일 23:44:17
+2026년 04월 03일 23:14:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,17 @@
+## 우선 key,value로 묶는 dictionary가 자료구조에서 먼저 떠오른다
+## 우선 len(clothes) 는 기본으로 무조건 입는거고
+## 추가로 이제 겹쳐입는 경우의 수는 곱셈으로 key의 개수의 곱셈으로
+## 최종답은 len(clothes) + key의 개수의 곱
 def solution(clothes):
-    hashmap = {}
-    for cloth, category in clothes:
-        if category not in hashmap:
-            hashmap[category] = []
-        hashmap[category].append(cloth)
-    
+    d = {}
     answer = 1
-    for category in hashmap:
-        answer *= (len(hashmap[category]) + 1)  # 입거나 안 입거나
     
-    return answer - 1  # 아무것도 안 입는 경우 제외
+    for c in clothes:
+        if c[1] not in d:
+            d[c[1]] = []   # 리스트 생성
+        d[c[1]].append(c[0])
+            
+    for v in d.values():
+        answer *= (len(v) + 1)
+        
+    return answer-1
